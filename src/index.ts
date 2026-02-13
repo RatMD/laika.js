c3ccd00c8499efab0b493b08d1d6fd1ccaf95cb9
sam <sam@rat.md>
2026-02-13 12:48:05 +0100

    [FIX] export

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { Head } from './components/head';
 export { PageComponent } from './components/page-component';
 export { PageContent } from './components/page-content';
 export { ProgressBar } from './components/progress-bar';
+export { useComponent } from './plugins/use-component';
 export { useOctober } from './plugins/use-october';
 export { useRouter } from './plugins/use-router';
 export { getProgressBar } from './plugins/get-progress-bar';
